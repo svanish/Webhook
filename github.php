960fbeae85bf01ae79e71603a90418ea2cd6417f
svanish <anishsv2009@gmail.com>
2014-02-06 23:38:04 +0530

    second commit

--- github.php
+++ github.php
@@ -6,7 +6,7 @@
       
       $response = $client->get($api . '/' . $id . '?access_token=' . $githubToken)->send();
 
-    
+    //New line
       $headers  = 'MIME-Version: 1.0' . "\r\n";
       $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
       $headers .= 'From: ' . $commit->author->name . ' <' . $from . '>' . "\r\n";
